ea285a523baa00233d9b3dd995ba4a07bb8f5661
b4its <exynosfam@gmail.com>
2025-12-15 10:14:11 +0800

    feat: add dark and light mode toggle theme

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,13 @@
 import { LoginForm } from "@/components/login-form"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
+        <div className="flex justify-end mb-2">
+          <ModeToggle />
+        </div>        
         <LoginForm />
       </div>
     </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle"
 import { SignupForm } from "@/components/signup-form"
 
 export default function SignupPage() {
   return (
+    
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    
       <div className="w-full max-w-sm md:max-w-4xl">
+      <div className="flex justify-end mb-2">
+        <ModeToggle />
+      </div>  
         <SignupForm />
       </div>
     </div>
